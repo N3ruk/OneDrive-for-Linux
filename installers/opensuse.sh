d2da37c79deb9f9c5e435cd25806f4cf311dda4b
N3ruk <neruk123@gmail.com>
2026-09-21 23:04:31 +0200

    feat: add universal Linux installer profiles

---
+++ installers/opensuse.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+. "$(dirname "${BASH_SOURCE[0]}")/common.sh"
+log "Instalando dependencias para openSUSE..."
+sudo_cmd zypper --non-interactive install \
+  rclone python3 python3-requests python3-gobject \
+  typelib-1_0-Gtk-3_0 libappindicator3-1 typelib-1_0-AppIndicator3-0_1 \
+  xdg-utils libnotify-tools fuse3
